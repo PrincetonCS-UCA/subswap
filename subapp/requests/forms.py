3c0bd774ff50f855ec3c27a49e20550472ca5692
Moin <moin@princeton.edu>
2022-04-01 11:34:45 -0400

    merged with admin

--- subapp/requests/forms.py
+++ subapp/requests/forms.py
@@ -10,6 +10,6 @@ class RequestForm(FlaskForm):
     """
     date_requested = DateField('Date Requested', format='%Y-%m-%d', validators=[DataRequired()])
     isSwap = RadioField(coerce=bool, choices=[(True, 'Yes'), (False, 'No')], validators=[DataRequired()])
-    bonus = IntegerField("Bonus", validators=[DataRequired(), NumberRange(min=0)])
+    bonus = IntegerField("Bonus")
     submit = SubmitField('Submit')
     
