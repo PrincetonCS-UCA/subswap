0998e212ccec14b55d819c59fd26aed601b4f10f
Moin <moin@princeton.edu>
2022-11-30 16:13:08 -0500

    config update

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 import os
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     ENV = 'development'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace("://", "ql://", 1)
+    if os.getenv('DATABASE_URL'):
+        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace(
+            "postgres://", "postgresql://", 1)
+    else:
+        SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, 'instance', 'app.db')}"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
